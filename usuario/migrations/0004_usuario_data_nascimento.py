1656492bbb3558a24186e8cd98993d888268b9e0
allaff <allaff.ribeiro@academico.ifpb.edu.br>
2022-10-29 14:37:58 -0300

    commit test

--- usuario/migrations/0004_usuario_data_nascimento.py
+++ usuario/migrations/0004_usuario_data_nascimento.py
@@ -10,9 +10,4 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AddField(
-            model_name='usuario',
-            name='data_nascimento',
-            field=models.DateField(null=True),
-        ),
     ]
