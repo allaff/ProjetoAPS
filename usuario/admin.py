baf8fb91351bb75ed7e9c54e4e1ea3812a0ab2ed
allaff <allaff.ribeiro@academico.ifpb.edu.br>
2022-10-29 09:08:49 -0300

    Configuração do super usuário

--- usuario/admin.py
+++ usuario/admin.py
@@ -8,17 +8,10 @@ def ativar_todos(modeladmin, request, queryset):
 @admin.action(description='Desativar todos os cadastros selecionados')
 def desativar_todos(modeladmin, request, queryset):
     queryset.update(ativo=False)
-class usuarioAdmin(admin.ModelAdmin):
-    list_display = [
-          'ativo',
-          'data_nascimento'
-    ]
-    search_fields = [
-        'nome'
-    ]
-    actions = [
-        ativar_todos,
-        desativar_todos
-    ]
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ['nome', 'email', 'senha', 'cpf', 'data_nascimento', 'telefone', 'endereco', 'ativo']
+    list_filter = ['ativo', 'data_nascimento']
+    search_fields = ['nome']
+    actions = [ativar_todos, desativar_todos]
 
-admin.site.register(Usuario)
+admin.site.register(Usuario, UsuarioAdmin)
